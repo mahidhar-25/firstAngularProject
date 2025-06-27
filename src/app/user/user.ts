35d78f434d2cadc6e7ca9f7d455dc482cbfcb202
mahidhar reddy <mahidhar100008@gmail.com>
2025-06-27 06:08:52 +0530

    used singlas and computed

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-user';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,5 +9,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class UserComponenet {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => {
+    return `/users/${this.selectedUser().avatar}`;
+  });
+  // This computed property will automatically update when selectedUser changes
+
+  onSelectUser() {
+    this.selectedUser.set(
+      DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]
+    );
+    console.log('Selected User:', this.selectedUser);
+    // Here you can add logic to handle the selected user, like navigating to a user profile
+  }
 }
