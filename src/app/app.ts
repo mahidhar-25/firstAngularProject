b875cf1c81ada82586706010b501c79be1b44f55
mahidhar reddy <mahidhar100008@gmail.com>
2025-06-27 05:40:59 +0530

    user component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header';
+import { HeaderComponent } from './header/header';
+import { UserComponenet } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponenet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
